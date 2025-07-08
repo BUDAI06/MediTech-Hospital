8190f7ff7f0ca4cce78e4d8bd9577a835d927c18
BUDAI06 <mauricio.vallejo1@udea.edu.co>
2025-07-08 07:21:18 -0500

    Update conexion_mysql.py

--- Proyecto_Mant_Biomedicos/src/conexion_mysql.py
+++ Proyecto_Mant_Biomedicos/src/conexion_mysql.py
@@ -3,11 +3,11 @@ import mysql.connector
 def conectar_mysql():
     try:
         conexion = mysql.connector.connect(
-            host="localhost",             # O "127.0.0.1"
-            port=3306,                    # Cambia si tienes otro puerto configurado
-            user="root",          # Usuario de tu base de datos
-            password= "",          # Contraseña
-            database="PF_Informatica1"    # Nombre de la base de datos
+            host="localhost",            
+            port=3306,                    
+            user="root",         
+            password= "",          
+            database="PF_Informatica1"    
         )
         return conexion
     except mysql.connector.Error as e:
